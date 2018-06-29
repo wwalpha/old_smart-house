17c972121683a1e60dd46b1584813fa68b6091d9
陶 金 <kinto@dcs.co.jp>
2018-06-29 15:07:31 +0900

    update firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+export default {
+  // apiKey: "<API_KEY>",
+  // authDomain: "<PROJECT_ID>.firebaseapp.com",
+  // databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+  // storageBucket: "<BUCKET>.appspot.com",
+  // messagingSenderId: "<SENDER_ID>",
+  apiKey: 'AIzaSyD2KuX2mpYg63sSUcVqzeR87jNGOvkXfTU',
+  authDomain: 'iot-home-chat.firebaseapp.com',
+  databaseURL: 'https://iot-home-chat.firebaseio.com',
+  storageBucket: 'iot-home-chat.appspot.com',
+  // messagingSenderId: "自身のconfig"
+};

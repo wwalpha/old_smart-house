d03cc83197af8c6f0b37e8df03b9eed87b4e9da5
wwalpha <wwalpha@gmail.com>
2018-06-30 17:39:57 +0900

    webpack config merge

---
+++ webpack/webpack.base.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HappyPack = require('happypack');
+
+module.exports = {
+  entry: [
+    './index.tsx',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      src: path.resolve(__dirname, '../src/'),
+      fb: path.resolve(__dirname, '../src/firebase/'),
+      utils: path.resolve(__dirname, '../src/utils/'),
+      components: path.resolve(__dirname, '../src/components/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              plugins: [
+                '@babel/plugin-syntax-typescript',
+                ["@babel/plugin-syntax-decorators", { "legacy": true }],
+                '@babel/plugin-syntax-jsx',
+                'react-hot-loader/babel',
+              ],
+            },
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              happyPackMode: true
+            }
+          }
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'source-map-loader',
+        enforce: 'pre',
+      },
+    ]
+  },
+  plugins: [
+    new HappyPack({
+      loaders: ['babel-loader', 'ts-loader'],
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'GoogleHome Chat',
+      filename: 'index.html',
+      template: path.join(__dirname, '../index.template.ejs'),
+      minify: false,
+      hash: true,
+      inject: 'body',
+    }),
+    new webpack.LoaderOptionsPlugin({
+      debug: false,
+    }),
+  ],
+  bail: true,
+};
+
--- webpack/webpack.mobile.js
+++ webpack/webpack.mobile.js
@@ -1,79 +1,19 @@
 const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HappyPack = require('happypack');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base');
 
-module.exports = {
+const mobile = {
   mode: 'development',
   devtool: 'cheap-module-eval-source-map',
-  entry: [
-    './index.tsx',
-  ],
   output: {
-    filename: 'bundle.js',
-    // path: path.resolve(__dirname, '../cordova/platforms/ios/www'),
-    path: path.resolve(__dirname, '../cordova/www'),
+    path: path.resolve(__dirname, '../cordova/platforms/ios/www'),
     publicPath: '',
   },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-    alias: {
-      src: path.resolve(__dirname, '../src/'),
-      fb: path.resolve(__dirname, '../src/firebase/'),
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              cacheDirectory: true,
-              plugins: [
-                '@babel/plugin-syntax-typescript',
-                ["@babel/plugin-syntax-decorators", { "legacy": true }],
-                '@babel/plugin-syntax-jsx',
-                'react-hot-loader/babel',
-              ],
-            },
-          },
-          {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true,
-              happyPackMode: true
-            }
-          }
-        ],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'source-map-loader',
-        enforce: 'pre',
-      },
-    ],
-  },
-  plugins: [
-    new HappyPack({
-      loaders: ['babel-loader', 'ts-loader']
-    }),
-    new webpack.EnvironmentPlugin('MOBILE'),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'GoogleHome Chat',
-      filename: 'index.html',
-      template: path.join(__dirname, '../index.template.ejs'),
-      minify: false,
-      hash: true,
-      inject: 'body',
-    }),
-    new webpack.LoaderOptionsPlugin({
-      debug: false,
-    }),
-  ],
-  bail: true,
 };
+
+const merged = merge(baseConfig, mobile);
+
+console.log(merged);
+
+module.exports=merged;
+

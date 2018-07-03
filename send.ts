1dff908ea81f3eee4746d9a203e5a2e971be9ee9
wwalpha <wwalpha@gmail.com>
2018-07-03 23:38:06 +0900

    o

--- send.ts
+++ send.ts
@@ -4,6 +4,6 @@ import * as moment from 'moment';
 // firebaseDb.ref('messages').child('-LGUtZMCTDeerfCUjZ3P').remove();
 
 firebaseDb.ref('messages').push({
-  body: '444444',
+  wav: 'https://s3-ap-northeast-1.amazonaws.com/iot-home-chat/20180703223901.wav',
   timestamp: moment().utc().format(),
 });

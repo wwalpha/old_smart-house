1a6a9e2ec459de665973e0f2537b611d2682f081
wwalpha <wwalpha@gmail.com>
2018-07-06 09:31:52 +0900

    Update config.ts

--- src/utils/firebase/config.ts
+++ src/utils/firebase/config.ts
@@ -4,7 +4,7 @@ export default {
   // databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
   // storageBucket: "<BUCKET>.appspot.com",
   // messagingSenderId: "<SENDER_ID>",
-  apiKey: 'AIzaSyD2KuX2mpYg63sSUcVqzeR87jNGOvkXfTU',
+  apiKey: 'BIzaSyD2KuX2mpYg63sSUcVqzeR87jNGOvkXfTU',
   authDomain: 'iot-home-chat.firebaseapp.com',
   databaseURL: 'https://iot-home-chat.firebaseio.com',
   storageBucket: 'iot-home-chat.appspot.com',

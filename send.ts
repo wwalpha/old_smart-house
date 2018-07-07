3149f382c95a17746881aa138ad094fde98b1ef5
wwalpha <wwalpha@gmail.com>
2018-07-07 10:26:46 +0900

    remove

--- send.ts
+++
@@ -1,9 +0,0 @@
-import { firebaseDb } from './src/utils/firebase/firebase';
-import * as moment from 'moment';
-
-// firebaseDb.ref('messages').child('-LGUtZMCTDeerfCUjZ3P').remove();
-
-firebaseDb.ref('messages').push({
-  wav: 'https://s3-ap-northeast-1.amazonaws.com/iot-home-chat/20180703223901.wav',
-  timestamp: moment().utc().format(),
-});

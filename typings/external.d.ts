e1474dc34168d07ac47e6f536bde277d8a910252
wwalpha <wwalpha@gmail.com>
2018-07-07 18:32:03 +0900

    add typings

--- typings/external.d.ts
+++ typings/external.d.ts
@@ -6,3 +6,4 @@
 /// <reference path="../node_modules/cordova-plugin-device/types/index.d.ts" />
 /// <reference path="../node_modules/cordova-plugin-file/types/index.d.ts" />
 /// <reference path="../node_modules/aws-amplify/lib/index.d.ts" />
+/// <reference path="../node_modules/aws-sdk/index.d.ts" />
